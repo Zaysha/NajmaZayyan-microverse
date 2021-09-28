b55da7bc79fe6a598bd06c2e0ba7b92eb3601b13
najma100 <64690646+najma100@users.noreply.github.com>
2021-09-28 04:45:33 -0700

    Add files via upload

---
+++ function utopianTree(n).js
@@ -0,0 +1,18 @@
+function utopianTree(n) {
+
+  
+        
+        for (let i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                height *= 2
+            } else {
+                height += 1;
+            }
+        }
+       
+    return height;
+
+
+     
+
+}
